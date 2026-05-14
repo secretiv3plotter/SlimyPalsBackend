8e638fd7538a4e2052279f3524cbf97f76d2cec6
secretiv3plotter <joshuaalinsolorin@gmail.com>
2026-05-15 05:25:07 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -14,11 +14,11 @@ const notificationRoutes = require('./routes/notificationRoutes');
 const app = express();
 
 // Middleware
-app.use(helmet());
 app.use(cors({
   origin: process.env.CORS_ORIGIN || '*',
   credentials: true
 }));
+app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json());
 
